055fa2610648378e2f9ac180f48860dac76fa2c8
bRexx [Bijen Maharjan] <bij.maharjan@gmail.com>
2017-12-27 18:51:11 +0545

    day11

--- HW5.php
+++ HW5.php
@@ -13,40 +13,42 @@ if (!function_exists('myStrRepeat')) {
      */
     function myStrRepeat($item, $num)
     {
-        $str='';
+        $str = '';
         for ($i = 0; $i < $num; $i++) {
             $str .= $item;
         }
-    return $str;
+        return $str;
     }
 }
 echo '<br>';
-echo myStrRepeat('apple',15);
+echo myStrRepeat('apple', 15);
 
 echo "<hr>";
 
 echo "No repeated number in the list <br> <pre>";
 
-if(!function_exists('unrepeatedArray')){
+if (!function_exists('unrepeatedArray')) {
     /**
      * function to create unique list of array
      * @param array ...$arr
      * @return array
      */
-    function unrepeatedArray(...$arr){
+    function unrepeatedArray(...$arr)
+    {
         print_r($arr);
         echo '<br> Ans: ';
-        $out=[];
-        $j=0;
-        for($i=0;$i<count($arr);$i++){
-                if(!in_array($arr[$i],$out)){
-                    $out[$j]=$arr[$i];
-                    $j++;
-                }
+        $out = [];
+        $j = 0;
+        for ($i = 0; $i < count($arr); $i++) {
+            if (!in_array($arr[$i], $out)) {
+                $out[$j] = $arr[$i];
+                $j++;
             }
-       return $out;
+        }
+        return $out;
     }
 }
 
-$arr = unrepeatedArray(1,2,3,3,4,5,3,4,5,12,3,34,23);
+$arr = unrepeatedArray(1, 2, 3, 3, 4, 5, 3, 4, 5, 12, 3, 34, 23);
 print_r($arr);
+
